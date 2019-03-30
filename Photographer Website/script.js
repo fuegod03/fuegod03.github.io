13c5d14c10f443e4a75a7ab1dce49187cdece2cc
fuegod03 <37029863+fuegod03@users.noreply.github.com>
2019-03-29 22:38:44 -0400

    Add files via upload

---
+++ Photographer Website/script.js
@@ -0,0 +1,28 @@
+$("#view-work").on("click", function(){
+const images = $("#images").position().top;
+
+$("html,body").animate(
+  {
+    scrollTop: images
+  },
+  900
+);
+
+});
+
+
+// var homeimg= document.querySelector(".font").querySelector(".fas")
+// console.log(homeimg)
+// var home = document.querySelector(".menu").querySelector(".home")
+
+// var menubar = document.querySelector(".menu")
+
+// var menuElement = document.querySelector(".menu").getElementsByTagName("li")
+
+
+// Pricing
+
+
+
+var bart= document.querySelector("#about-menu")
+console.log(bart)
